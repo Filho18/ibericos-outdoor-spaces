07b3221c12c07bafbfc825a586c062243ee7b28b
Filho18 <albinofilhoportugal@gmail.com>
2025-05-14 09:40:00 +0100

    Update ContactForm.tsx

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -9,7 +9,7 @@ const ContactForm = () => {
       <h3 className="text-2xl font-semibold mb-6 text-iberico-800">Peça um Orçamento</h3>
       
       <form action="https://formsubmit.co/el/jegaso" method="POST" className="space-y-4">
-        <input type="hidden" name="_next" value="https://meusite.com/sucesso" />
+        <input type="hidden" name="_next" value="https://google.com" />
         <input type="hidden" name="_captcha" value="false" />
         <Input type="text" name="nome" placeholder="Seu nome" required />
         <Input type="email" name="email" placeholder="Seu e-mail" required />
