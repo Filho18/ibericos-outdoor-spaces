8916fa78a8282844ba64c74b2f6e738893850b4a
Filho18 <albinofilhoportugal@gmail.com>
2025-06-27 03:07:00 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -85,8 +85,8 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3 text-iberico-300 shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <a href="mailto:casadostoldos@gmail.com" className="text-iberico-300 hover:text-white transition-colors">
-                  casadostoldos@gmail.com
+                <a href="" className="text-iberico-300 hover:text-white transition-colors">
+                  
                 </a>
               </li>
             </ul>
